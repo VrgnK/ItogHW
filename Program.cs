0152198c865494c790af4a39a8e453c02801a251
Vurgun <slayer13_86@mail.ru>
2024-03-17 00:01:24 +0800

    код выдает вопрос о количестве элементов массива и предлагает их ввести

--- Program.cs
+++ Program.cs
@@ -1,3 +1,59 @@
-﻿/// сформировал блок-схему кода 
-string imagePath = "obj/BlokEnd.png";
+﻿using System;
 
+
+
+
+    class Program
+    {
+    static void Main()
+    {
+    /// сформировал блок-схему кода 
+    ///string imagePath = "obj/BlokEnd.png";
+    {
+        Console.Write("Введите количество элементов в массиве: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[] array = new string[n];
+
+ for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент {i + 1}: ");
+            array[i] = Console.ReadLine();
+        }
+
+        string[] result = FilterArray(array);
+
+        Console.WriteLine("Результат:");
+        foreach (string str in result)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static string[] FilterArray(string[] array)
+    {
+        int count = 0;
+
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+                
+        string[] result = new string[count];
+                int index = 0;
+        foreach (string str in array)
+        {
+            if (str.Length <= 3)
+            {
+                result[index] = str;
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
+}
